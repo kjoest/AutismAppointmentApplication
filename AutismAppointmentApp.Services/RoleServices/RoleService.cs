e4cc2987c1cf24f8c03aa470d3463d19f6d3440f
kyle Joest <kjoest92@gmail.com>
2022-06-18 15:52:56 -0500

    Made Admin changes

--- AutismAppointmentApp.Services/RoleServices/RoleService.cs
+++ AutismAppointmentApp.Services/RoleServices/RoleService.cs
@@ -26,8 +26,8 @@ namespace AutismAppointmentApp.Services.RoleServices
         {
             ApplicationDbContext ctx = new ApplicationDbContext();
             UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
-
-            var myUser = ctx.Users.SingleOrDefault(u => u.Email == "kyle@1234.com");
+                                                                    //Password = Admin123!
+            var myUser = ctx.Users.SingleOrDefault(u => u.Email == "admin@autism.com");
             if (myUser != null)
             {
                 var adminRes = userManager.AddToRole(myUser.Id, "Admin");
